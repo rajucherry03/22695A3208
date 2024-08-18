9616e820ad2727aeb410e8db5a380d51580f0d42
S Raju <115474985+rajucherry03@users.noreply.github.com>
2024-08-18 13:23:07 +0530

    Delete task1/Task2 directory

--- task1/Task2/items/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import ProductListView, ProductDetailView
-
-urlpatterns = [
-    path('categories/<str:categoryname>/products/', ProductListView.as_view(), name='product-list'),
-    path('categories/<str:categoryname>/products/<str:productid>/', ProductDetailView.as_view(), name='product-detail'),
-]
